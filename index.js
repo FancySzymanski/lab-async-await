4b9e5d346e1abb1f4b77f30a51fc3ed75f3f5795
Alex Szymanski <alex.szy.2@gmail.com>
2026-07-18 19:45:55 -0400

    Added Code

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your code here!
+function displayPosts(posts) {
+    const ul = document.getElementById("post-list");
+
+    posts.forEach(function (post) {
+        const li = document.createElement("li");
+        const h1 = document.createElement("h1");
+        const p = document.createElement("p");
+    
+        h1.textContent = post.title;
+        p.textContent = post.body;
+    
+        li.appendChild(h1)
+        li.appendChild(p)
+        ul.appendChild(li)
+    });
+}
+
+async function fetchAndDisplayPosts() {
+    try {
+        const response = await 
+        fetch('https://jsonplaceholder.typicode.com/posts');
+
+        const posts = await response.json();
+
+        displayPosts(posts)
+    } catch (error) {
+        const errorMessage = document.createElement("li")
+        errorMessage.textContent = 'Failed to load posts, please try again later'
+        document.getElementById("post-list").appendChild(errorMessage)
+    }
+}
+fetchAndDisplayPosts();
